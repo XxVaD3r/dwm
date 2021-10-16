ed434d526e320fbd4e2954d4f86aab6ae59c515e
XxVaD3r <dima.simmons435@gmail.com>
2021-10-15 21:31:37 -0600

    test patches

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "dejavu sans:size=10" };
-static const char dmenufont[]       = "monospace:size=12";
+static const char dmenufont[]       = "dejavu sans:size=12";
 static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
